d8c203f29882cf8806dcd473c18a9f6f121e159a
Cata Allende <franallende2000@gmail.com>
2024-10-10 19:38:47 -0300

    todo terminado

--- src/screens/splash/splash.js
+++ src/screens/splash/splash.js
@@ -1,34 +1,52 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {AppColors} from '../../assets/styles/default-styles';
 
-const SplashScreen = ({ navigation }) => {
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('Login');
+    }, 3000); // 3 segundos de duración
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            navigation.replace('Login');
-        }, 1000);
+    return () => clearTimeout(timer);
+  }, [navigation]);
 
-        return () => clearTimeout(timer);
-    }, [navigation]);
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Bienvenido a Relevamiento Visual</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('./../../assets/img/edificios.png')}
+        style={styles.icon}
+      />
+      <Text style={styles.title}>Relevamiento Visual</Text>
+      <Text style={styles.info}>Francisco Allende</Text>
+      <Text style={styles.info}>División A141-2</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#673AB7',
-    },
-    text: {
-        fontSize: 24,
-        color: 'white',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5D5D61',
+  },
+  icon: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 20,
+  },
+  info: {
+    fontSize: 18,
+    color: 'white',
+    marginBottom: 10,
+  },
 });
 
 export default SplashScreen;
